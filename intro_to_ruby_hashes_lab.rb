fc2090e91827a9d82b5a116e0709bfc859568d23
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 16:52:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,6 +52,7 @@ end
 
 def monopoly_with_fourth_tier
  :railroads => {
+   
 	    :pieces => 4,
 	    
 	     :names => {
@@ -66,7 +67,7 @@ def monopoly_with_fourth_tier
 	       :two_pieces_owned => 50,
 	       :three_pieces_owned => 100,
 	       :four_pieces_owned => 200
-	     },
+	     }
 	     
 	  }
 	
